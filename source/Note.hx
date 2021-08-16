a6d4285e91532374b4382624295678a33f62c44e
Rozebud <allofthebonusducks@gmail.com>
2021-08-16 14:55:13 -0400

    Hold Note Fix
    Fixes an issue that made you have to hold down held notes for longer than required.

--- source/Note.hx
+++ source/Note.hx
@@ -201,7 +201,7 @@ class Note extends FlxSprite
 		if (mustPress)
 		{
 			if(isSustainNote){
-				canBeHit = (strumTime < Conductor.songPosition /*+ Conductor.safeZoneOffset * 0.25*/);
+				canBeHit = (strumTime < Conductor.songPosition + Conductor.safeZoneOffset * 0.125);
 			}
 			else{
 				canBeHit = (strumTime > Conductor.songPosition - Conductor.safeZoneOffset
