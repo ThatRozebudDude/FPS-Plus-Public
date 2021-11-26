b6a85220caefea06dc0994146d82a6fe742df04c
Smokey <78874128+Smokey555@users.noreply.github.com>
2021-11-26 02:28:34 +0100

    Add files via upload

---
+++ source/MikuSoundSystem.hx
@@ -0,0 +1,54 @@
+package;
+
+import flixel.FlxG;
+import openfl.media.SoundTransform;
+import flixel.tweens.FlxTween;
+import openfl.media.Sound;
+import openfl.media.SoundChannel;
+import openfl.Assets;
+
+class MikuSoundSystem
+{
+	public var sound:Sound;
+	var channel:SoundChannel;
+	var fadeTween:FlxTween;
+	var volume:Float = 1;
+
+	public function new()
+	{
+		sound = new Sound();
+	}
+
+	public function loadSound(key:String, cache:Bool = true)
+	{
+		if (sound != null)
+			sound = Assets.getMusic(key, cache);
+		else
+			trace('sound is null dickhead');
+	}
+
+	public function play()
+	{
+		channel = sound.play();
+		channel.soundTransform = new SoundTransform(FlxG.sound.volume);
+	}
+
+	public function changeVolume(vol:Float)
+	{
+		if (channel != null)
+			channel.soundTransform = new SoundTransform(vol);
+
+		volume = vol;
+	}
+
+	public function stop()
+	{
+		if (channel != null)
+		{
+			channel.stop();
+			channel = null;
+		}
+		else
+			trace('No sound found in the channel!');
+	}
+}
