4ac0d6f016dc78d700d733384c951130073baf5d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-02 10:30:35 +0200

    Don't overload the assets please

--- source/ImageCache.hx
+++ source/ImageCache.hx
@@ -8,19 +8,18 @@ class ImageCache
 
 	public static function add(path:String):Void
 	{
-		var data:FlxGraphic = FlxGraphic.fromBitmapData(GPUBitmap.create(path));
-		data.persist = true;
-		data.destroyOnNoUse = false;
-		cache.set(path, data);
+		if (!cache.exists(path))
+		{
+			var data:FlxGraphic = FlxGraphic.fromBitmapData(GPUBitmap.create(path));
+			data.persist = true;
+			data.destroyOnNoUse = false;
+			cache.set(path, data);
+		}
 	}
 
 	public static function get(path:String):FlxGraphic
-	{
 		return cache.get(path);
-	}
 
 	public static function exists(path:String)
-	{
 		return cache.exists(path);
-	}
 }
