b77fbac29922350713824c5f4bd72f9c721224a4
SIG7 <82591291+SIG7Pro@users.noreply.github.com>
2023-04-04 00:27:00 -0400

    Updated it so that the icons are properly done.

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -15,40 +15,52 @@ class HealthIcon extends FlxSprite
 
 	private var tween:FlxTween;
 
-	private static final pixelIcons:Array<String> = ["bf-pixel", "senpai", "senpai-angry", "spirit"];
+	//private static final pixelIcons:Array<String> = ["bf-pixel", "senpai", "senpai-angry", "spirit"];
 
 	public function new(char:String = 'face', isPlayer:Bool = false, ?_id:Int = -1)
 	{
 		super();
 		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
 			
-		animation.add('bf', [0, 1, 30], 0, false, isPlayer);
-		animation.add('bf-car', [0, 1, 30], 0, false, isPlayer);
-		animation.add('bf-christmas', [0, 1, 30], 0, false, isPlayer);
-		animation.add('bf-pixel', [21, 41, 40], 0, false, isPlayer);
-		animation.add('spooky', [2, 3, 31], 0, false, isPlayer);
-		animation.add('pico', [4, 5, 32], 0, false, isPlayer);
-		animation.add('mom', [6, 7, 33], 0, false, isPlayer);
-		animation.add('mom-car', [6, 7, 33], 0, false, isPlayer);
-		animation.add('tankman', [8, 9, 50], 0, false, isPlayer);
-		animation.add('face', [10, 11, 38], 0, false, isPlayer);
-		animation.add('dad', [12, 13, 34], 0, false, isPlayer);
-		animation.add('senpai', [22, 42, 43], 0, false, isPlayer);
-		animation.add('senpai-angry', [44, 45, 46], 0, false, isPlayer);
-		animation.add('spirit', [23, 47, 48], 0, false, isPlayer);
-		animation.add('bf-old', [14, 15, 39], 0, false, isPlayer);
-		animation.add('parents-christmas', [17, 18, 36], 0, false, isPlayer);
-		animation.add('monster', [19, 20, 37], 0, false, isPlayer);
-		animation.add('monster-christmas', [19, 20, 37], 0, false, isPlayer);
-		animation.add('gf', [16, 49, (_id != -1) ? 49 : 35], 0, false, isPlayer);
-		animation.add('gf-car', [16, 49, 35], 0, false, isPlayer);
-		animation.add('gf-pixel', [16, 49, 35], 0, false, isPlayer);
+		//BF Default Icons
+		animation.add('bf', [0, 1, 2], 0, false, isPlayer);
+		animation.add('bf-car', [0, 1, 2], 0, false, isPlayer);
+		animation.add('bf-christmas', [0, 1, 2], 0, false, isPlayer);
+		animation.add('bf-pixel', [0, 1, 2], 0, false, isPlayer);
+		
+		//Ivan Icons
+		animation.add('ivan-stressed', [3, 4, 5], 0, false, isPlayer);
+		animation.add('ivan-twoface', [6, 7, 8], 0, false, isPlayer);
+		
+		// epic cawth woman
+		animation.add('cawth', [9, 10, 11], 0, false, isPlayer);
+		// okay enough cawth appreciation
+		
+		//Dumb Ivan Icon
+		animation.add('ivan-em', [12, 13, 14], 0, false, isPlayer);
+		
+		//The best Easter Egg in the game.
+		animation.add('bf-old', [15, 16, 17], 0, false, isPlayer);
+		
+		//Other icons.
+		animation.add('sink', [18, 18, 18], 0, false, isPlayer);
+		animation.add('face', [20, 20, 20], 0, false, isPlayer);
+		animation.add('emily-silly', [19], 0, false, isPlayer);
+		animation.add('lordX', [21, 22, 23], 0, false, isPlayer);
+		
+		// GF Has no icon, so we give her the icon of No. #20
+		animation.add('gf', [20, 20, 20], 0, false, isPlayer);
+		animation.add('gf-car', [20, 20, 20], 0, false, isPlayer);
+		animation.add('gf-pixel', [20, 20, 20], 0, false, isPlayer);
+		
+		
+		// To do: load fail-safe.
 
 		iconSize = width;
 
 		id = _id;
 		
-		antialiasing = !pixelIcons.contains(char);
+		antialiasing = false;
 		animation.play(char);
 		scrollFactor.set();
 
