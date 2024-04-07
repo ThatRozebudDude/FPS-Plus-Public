9b1437a6918b53d7c5ac1f97af34e0616dc0367d
Rozebud <allofthebonusducks@gmail.com>
2024-04-07 03:54:41 -0400

    extended tree sprites

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -329,7 +329,7 @@ class ChartingState extends MusicBeatState
 		Conductor.changeBPM(_song.bpm);
 		Conductor.mapBPMChanges(_song);
 
-		bpmTxt = new FlxText(1140, 50, 0, "", 16);
+		bpmTxt = new FlxText(1140, 50, 0, "", 12);
 		bpmTxt.scrollFactor.set();
 		add(bpmTxt);
 
