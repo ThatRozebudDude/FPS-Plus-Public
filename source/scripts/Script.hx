f072f5a9ad6ca7951275b010ff262290c6b8186e
soushimiya <131571752+soushimiya@users.noreply.github.com>
2024-11-05 00:42:30 +0900

    comments and cleanups

--- source/scripts/Script.hx
+++ source/scripts/Script.hx
@@ -5,39 +5,42 @@ import hscript.Interp;
 
 using StringTools;
 
+/**
+	Enhanced Hscript Interpreter that supports importing Package, get Object from parent instance...
+	@author Soushimiya
+**/
+
 class Script extends Interp
 {
     public var parent:Dynamic;
-    private var parser:Parser;
 
     override public function new(path:String){
         super();
         setVariables();
 
-        parser = new Parser();
+        var parser = new Parser();
         parser.allowJSON = parser.allowMetadata = parser.allowTypes = true;
         
-        var scriptStrings = Utils.getTextInLines(path);
-        
         var scriptToRun:String = "";
-        for (line in scriptStrings){
+        //custom "parser" cuz i'm so lazy
+        for (line in Utils.getTextInLines(path)){
             switch(line.split(" ")[0]){
                 case "import":
                     importClass(line.split("import ")[1].split(";")[0]);
                     line = "//import " + line.split("import ")[1];
             }
-            
+
             scriptToRun += line + "\n";
         }
 
-        var parsedScript = parser.parseString(scriptToRun);
-        execute(parsedScript);
+        execute(parser.parseString(scriptToRun));
     }
 
     //call Function in Interp
     public function callFunc(func:String, ?args:Array<Dynamic>):Dynamic {
 		if (variables.exists(func)) {
-			if (args == null) args = [];
+			if (args == null){ args = []; }
+
 			try {
 				return Reflect.callMethod(null, variables.get(func), args);
 			}
@@ -60,6 +63,7 @@ class Script extends Interp
         variables.set(className, Type.resolveClass(classPath));
 	}
 
+    //import some default classes
     private function setVariables(){
         importClass("Paths");
         importClass("Character");
@@ -74,7 +78,6 @@ class Script extends Interp
 		variables.set('remove', flixel.FlxG.state.remove);
     }
 
-    //get Parents var too
     override public function resolve(id:String):Dynamic {
 		if (parent != null) {
             var instanceFields = Type.getInstanceFields(Type.getClass(parent));
