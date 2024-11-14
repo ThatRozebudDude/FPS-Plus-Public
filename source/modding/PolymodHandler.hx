7fae52eeb928fda15f94be9fd665e186d6cb2551
soushimiya <131571752+soushimiya@users.noreply.github.com>
2024-11-14 09:35:27 +0900

    doing someshit lmao

--- source/modding/PolymodHandler.hx
+++ source/modding/PolymodHandler.hx
@@ -5,6 +5,7 @@ import haxe.Json;
 import polymod.PolymodConfig;
 import flixel.FlxG;
 import polymod.Polymod;
+import openfl.display.BlendMode;
 
 using StringTools;
 
@@ -237,6 +238,7 @@ class PolymodHandler
         Polymod.addDefaultImport(Paths);
         Polymod.addDefaultImport(flixel.group.FlxGroup);
         Polymod.addDefaultImport(flixel.group.FlxSpriteGroup);
+        Polymod.addDefaultImport(FlxTextBorderStyle);
 
         Polymod.addDefaultImport(PlayState);
         Polymod.addDefaultImport(GameOverSubstate);
@@ -276,6 +278,9 @@ class PolymodHandler
         //Alias
         Polymod.addImportAlias("lime.utils.Assets", Assets);
         Polymod.addImportAlias("openfl.utils.Assets", Assets);
+
+        Polymod.addImportAlias("flash.display.BlendMode", PolyBlendMode);
+        Polymod.addImportAlias("openfl.display.BlendMode", PolyBlendMode);
 
         Polymod.addImportAlias("flixel.math.FlxPoint", flixel.math.FlxPoint.FlxBasePoint);
 
@@ -342,4 +347,31 @@ enum ModError{
     MISSING_META_FIELDS;
     API_VERSION_TOO_OLD;
     API_VERSION_TOO_NEW;
+}
+
+private class PolyBlendMode
+{
+    public static var ADD = BlendMode.ADD;
+    public static var ALPHA = BlendMode.ALPHA;
+    public static var DARKEN = BlendMode.DARKEN;
+    public static var DIFFERENCE = BlendMode.DIFFERENCE;
+    public static var ERASE = BlendMode.ERASE;
+    public static var HARDLIGHT = BlendMode.HARDLIGHT;
+    public static var INVERT = BlendMode.INVERT;
+    public static var LAYER = BlendMode.LAYER;
+    public static var LIGHTEN = BlendMode.LIGHTEN;
+    public static var MULTIPLY = BlendMode.MULTIPLY;
+    public static var NORMAL = BlendMode.NORMAL;
+    public static var OVERLAY = BlendMode.OVERLAY;
+    public static var SCREEN = BlendMode.SCREEN;
+    public static var SHADER = BlendMode.SHADER;
+    public static var SUBTRACT = BlendMode.SUBTRACT;
+}
+
+class FlxTextBorderStyle
+{
+    public static var NONE = flixel.text.FlxText.FlxTextBorderStyle.NONE;
+    public static var SHADOW = flixel.text.FlxText.FlxTextBorderStyle.SHADOW;
+    public static var OUTLINE = flixel.text.FlxText.FlxTextBorderStyle.OUTLINE;
+    public static var OUTLINE_FAST = flixel.text.FlxText.FlxTextBorderStyle.OUTLINE_FAST;
 }
--- source/modding/ScriptingUtil.hx
+++ source/modding/ScriptingUtil.hx
@@ -17,41 +17,6 @@ using StringTools;
 
 class ScriptingUtil
 {
-
-    //Blend mode aliases so you don't have to use an integer in scripts.
-    public static var add(get, never):Int;
-    public static inline function get_add()         { return 0; }
-    public static var difference(get, never):Int;
-    public static inline function get_difference()  { return 3; }
-    public static var invert(get, never):Int;
-    public static inline function get_invert()      { return 6; }
-    public static var multiply(get, never):Int;
-    public static inline function get_multiply()    { return 9; }
-    public static var normal(get, never):Int;
-    public static inline function get_normal()      { return 10; }
-    public static var screen(get, never):Int;
-    public static inline function get_screen()      { return 12; }
-    public static var subtract(get, never):Int;
-    public static inline function get_subtract()    { return 14; }
-
-    //These ones aren't supported but are included for completeness.
-    public static var alpha(get, never):Int;
-    public static inline function get_alpha()       { return 1; }
-    public static var darken(get, never):Int;
-    public static inline function get_darken()      { return 2; }
-    public static var erase(get, never):Int;
-    public static inline function get_erase()       { return 4; }
-    public static var hardlight(get, never):Int;
-    public static inline function get_hardlight()   { return 5; }
-    public static var layer(get, never):Int;
-    public static inline function get_layer()       { return 7; }
-    public static var lighten(get, never):Int;
-    public static inline function get_lighten()     { return 8; }
-    public static var overlay(get, never):Int;
-    public static inline function get_overlay()     { return 11; }
-    public static var shader(get, never):Int;
-    public static inline function get_shader()      { return 13; }
-
     //FlxAxes aliases.
     public static var axisNone(get, never):Int;
     public static inline function get_axisNone()    { return 0x00; }
@@ -76,15 +41,6 @@ class ScriptingUtil
     public static inline function get_rankPerfect()     { return perfect; }
     public static var rankGold(get, never):Rank;
     public static inline function get_rankGold()        { return gold; }
-    
-    public static var borderStyleNone(get, never):FlxTextBorderStyle;
-    public static inline function get_borderStyleNone()         { return FlxTextBorderStyle.NONE; }
-    public static var borderStyleShadow(get, never):FlxTextBorderStyle;
-    public static inline function get_borderStyleShadow()       { return FlxTextBorderStyle.SHADOW; }
-    public static var borderStyleOutline(get, never):FlxTextBorderStyle;
-    public static inline function get_borderStyleOutline()      { return FlxTextBorderStyle.OUTLINE; }
-    public static var borderStyleOutlineFast(get, never):FlxTextBorderStyle;
-    public static inline function get_borderStyleOutlineFast()  { return FlxTextBorderStyle.OUTLINE_FAST; }
 
     public static inline function makeFlxGroup():FlxTypedGroup<FlxBasic>                { return new FlxTypedGroup<FlxBasic>(); }
     public static inline function makeFlxSpriteGroup():FlxTypedSpriteGroup<FlxSprite>   { return new FlxTypedSpriteGroup<FlxSprite>(); }
