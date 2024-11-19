97543fb68df45ad5c4c391030f21650d3954726a
Rozebud <allofthebonusducks@gmail.com>
2024-11-19 00:29:06 -0500

    re-add ids

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -47,7 +47,7 @@ class MainMenuState extends MusicBeatState
 	public static final lerpSpeed:Float = 0.01;
 	final warningDelay:Float = 10;
 
-	inline public static final VERSION:String = "6.1.1";
+	inline public static final VERSION:String = "6.1.2";
 	inline public static final NONFINAL_TAG:String = "(Non-Release Build)";
 	inline public static final SHOW_BUILD_INFO:Bool = true; //Set this to false when making a release build.
 
