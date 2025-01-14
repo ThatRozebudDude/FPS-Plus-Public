81534949f71f4da4a0b61d8d0cf1ff5b04990314
doggogit <andrejjosifovski5455@gmail.com>
2025-01-14 15:31:13 +0100

    Use lavycat's `lime` fork for actual uncapped framerate
    Also, instead of having two `cd ..` in the oxipng script, just have one

--- art/scripts/optimize_images.bat
+++ art/scripts/optimize_images.bat
@@ -2,8 +2,7 @@
 rem This script goes through every .png in the images folder and optimizes it to reduce file size.
 rem This requires oxipng to run. If you don't have it you can get it here: https://github.com/shssoichiro/oxipng/releases
 rem This will take a lil' bit to run so be prepared to wait while it does it's thing.
-cd..
-cd..
+cd../..
 cd assets/images
 for /R %%f in (*.png) do oxipng -o 6 --strip all --alpha "%%f"
 pause
