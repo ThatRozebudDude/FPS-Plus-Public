c3b22839426c8515ef474780f0a0af2f10518266
Rozebud <allofthebonusducks@gmail.com>
2025-04-08 01:28:23 -0400

    i have once again tweaked school intro instead of adding the rim light shader

--- source/shaders/MultiplyFadeShader.hx
+++ source/shaders/MultiplyFadeShader.hx
@@ -12,11 +12,13 @@ class MultiplyFadeShader extends FlxBasic
 
 	public var fadeVal(default, set):Float = 1;
 	public var power(default, set):Float = 1;
+	public var curvePower(default, set):Float = 1;
 
-	public function new(?_power:Float = 2, ?_fadeVal:Float = 1){
+	public function new(?_power:Float = 2, ?_fadeVal:Float = 1, ?_curvePower:Float = 1){
 		super();
 		power = _power;
 		fadeVal = _fadeVal;
+		curvePower = _curvePower;
 	}
 
 	function set_fadeVal(v:Float):Float{
@@ -30,6 +32,12 @@ class MultiplyFadeShader extends FlxBasic
 		shader.power.value = [power];
 		return v;
 	}
+
+	function set_curvePower(v:Float):Float {
+		curvePower = v;
+		shader.curvePower.value = [curvePower];
+		return v;
+	}
 }
 
 class MultiplyFadeShaderGLSL extends FlxShader
@@ -39,12 +47,13 @@ class MultiplyFadeShaderGLSL extends FlxShader
 
 		uniform float fadeAmt;
 		uniform float power;
+		uniform float curvePower;
 
 		void main(){
 			vec4 tex = flixel_texture2D(bitmap, openfl_TextureCoordv);
 			vec3 unMultColor = vec3(0.0);
 			if(tex.a > 0.0){ unMultColor = tex.rgb / tex.a; }
-			vec3 finalColor = mix(pow(unMultColor, vec3(power)) * fadeAmt, unMultColor * fadeAmt, fadeAmt);
+			vec3 finalColor = pow(unMultColor, vec3(power / mix(1.0, power, pow(fadeAmt, curvePower)))) * fadeAmt;
 			gl_FragColor = vec4(finalColor * tex.a, tex.a);
 		}
 
