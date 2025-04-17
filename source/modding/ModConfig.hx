13656a4a1083a00cad77551716bc097691ea59b9
Rozebud <35246975+ThatRozebudDude@users.noreply.github.com>
2025-04-17 16:10:08 -0400

    I DONT KNOW IF THIS WORKS IM NOT HOME

--- source/modding/ModConfig.hx
+++ source/modding/ModConfig.hx
@@ -1,11 +1,14 @@
 package modding;
 
+import sys.FileSystem;
+import sys.io.File;
+import haxe.Json;
 import flixel.FlxG;
 
 class ModConfig
 {
 
-	static var configMap:Map<String, Map<String, Dynamic>> = new MapMap<String, Map<String, Dynamic>>();
+	static var configMap:Map<String, Map<String, Dynamic>> = new Map<String, Map<String, ModSetting>>();
 
 	static function load():Void{
 		SaveManager.modConfig();
@@ -13,6 +16,61 @@ class ModConfig
 		if(FlxG.save.data.configMap != null){
 			configMap = FlxG.save.data.configMap;
 		}
+
+		for(dir in PolymodHandler.allModDirs){
+			if(!PolymodHandler.malformedMods.exists(dir)){
+				if(!FileSystem.exists("mods/" + dir + "/config.json")){
+					continue;
+				}
+				var meta = Json.parse(File.getContent("mods/" + dir + "/meta.json"));
+				var json = Json.parse(File.getContent("mods/" + dir + "/config.json"));
+
+				if(!configMap.exists(meta.uuid)){
+					configMap.set(meta.uuid, new Map<String, ModSetting>());
+				}
+
+				for(i in 0...json.config.length){
+					var setting:ModSetting;
+
+					switch(json.config[i].type){
+						case "bool":
+							setting = {
+								type: "bool",
+								value: json.config[i].properties.defaultBool
+							};
+						case "int":
+							setting = {
+								type: "int",
+								value: Std.int(json.config[i].properties.defaultValue)
+							};
+						case "float":
+							setting = {
+								type: "float",
+								value: json.config[i].properties.defaultValue
+							};
+						case "list":
+							setting = {
+								type: "list",
+								value: json.config[i].properties.values[Std.int(json.config[i].properties.defaultIndex)]
+							};
+						default:
+							trace("Unknown config type \"" + json.config[i].type + "\", skipping.");
+					}
+					
+					if(!configMap.get(meta.uuid).exists(json.config[i].name)){
+						configMap.get(meta.uuid).set(json.config[i].name, setting);
+					}
+					else{
+						//Should probably add more checks later to make sure stuff doesn't go out of bounds or whatever.
+						if(configMap.get(meta.uuid).get(json.config[i].name).type != setting.type){
+							configMap.get(meta.uuid).set(json.config[i].name, setting);
+						}
+					}
+				}
+			}
+		}
+
+		save();
 
 		SaveManager.previousSave();
 	}
@@ -25,8 +83,13 @@ class ModConfig
 
 	public static function get(uuid:String, name:String):Dynamic{
 		var r = configMap.get(uuid);
-		if(r != null){ r = r.get(name); }
+		if(r != null){ r = r.get(name).value; }
 		return r;
 	}
 
+}
+
+typedef ModSetting = {
+	type:String,
+	value:Dynamic
 }
