038b3d7e0c182873157b03409f2b8dc49ccfe5d3
Rozebud <allofthebonusducks@gmail.com>
2025-08-03 01:34:12 -0400

    change "isConfig" to "configParam"
    i like this term better >:]

--- source/config/Config.hx
+++ source/config/Config.hx
@@ -8,31 +8,31 @@ using StringTools;
 
 class Config
 {
-	@isConfig public static var offset:Float = 0.0;
-	@isConfig public static var healthMultiplier:Float = 1.0;
-	@isConfig public static var healthDrainMultiplier:Float = 1.0;
-	@isConfig public static var comboType:Int = 1;
-	@isConfig public static var downscroll:Bool = false;
-	@isConfig public static var noteGlow:Bool = true;
-	@isConfig public static var ghostTapType:Int = 0;
-	@isConfig public static var framerate:Int = 999;
-	@isConfig public static var bgDim:Int = 0;
-	@isConfig public static var noteSplashType:Int = 1;
-	@isConfig public static var centeredNotes:Bool = false;
-	@isConfig public static var scrollSpeedOverride:Float = -1;
-	@isConfig public static var showComboBreaks:Bool = false;
-	@isConfig public static var showFPS:Bool = false;
-	@isConfig public static var useGPU:Bool = true;
-	@isConfig public static var extraCamMovement:Bool = true;
-	@isConfig public static var camBopAmount:Int = 0;
-	@isConfig public static var showCaptions:Bool = true;
-	@isConfig public static var showAccuracy:Bool = true;
-	@isConfig public static var showMisses:Int = 1;
-	@isConfig public static var autoPause:Bool = true;
-	@isConfig public static var flashingLights:Bool = true;
+	@configParam public static var offset:Float = 0.0;
+	@configParam public static var healthMultiplier:Float = 1.0;
+	@configParam public static var healthDrainMultiplier:Float = 1.0;
+	@configParam public static var comboType:Int = 1;
+	@configParam public static var downscroll:Bool = false;
+	@configParam public static var noteGlow:Bool = true;
+	@configParam public static var ghostTapType:Int = 0;
+	@configParam public static var framerate:Int = 999;
+	@configParam public static var bgDim:Int = 0;
+	@configParam public static var noteSplashType:Int = 1;
+	@configParam public static var centeredNotes:Bool = false;
+	@configParam public static var scrollSpeedOverride:Float = -1;
+	@configParam public static var showComboBreaks:Bool = false;
+	@configParam public static var showFPS:Bool = false;
+	@configParam public static var useGPU:Bool = true;
+	@configParam public static var extraCamMovement:Bool = true;
+	@configParam public static var camBopAmount:Int = 0;
+	@configParam public static var showCaptions:Bool = true;
+	@configParam public static var showAccuracy:Bool = true;
+	@configParam public static var showMisses:Int = 1;
+	@configParam public static var autoPause:Bool = true;
+	@configParam public static var flashingLights:Bool = true;
 
-	@isConfig public static var ee1:Bool = false;
-	@isConfig public static var ee2:Bool = false;
+	@configParam public static var ee1:Bool = false;
+	@configParam public static var ee2:Bool = false;
 
 	static var configList(get, default):Array<String>;
 
@@ -65,19 +65,18 @@ class Config
 		if(useValueInsteadOfSave > -1){ fps = useValueInsteadOfSave; }
 		if(fps > cap) { fps = cap; }
 		FlxG.updateFramerate = fps;
-		FlxG.drawFramerate  = fps;
+		FlxG.drawFramerate = fps;
 	}
 
-	static function get_configList():Array<String>
-	{
-		if (configList != null){ // Preventing excessive Reflect calls
+	static function get_configList():Array<String>{
+		if(configList != null){ // Preventing excessive Reflect calls
 			return configList;
 		}
 
 		configList = [];
 
-		for (field in Type.getClassFields(Config)){
-			if (RestrictedUtils.hasMetadata(Config, field, "isConfig")){
+		for(field in Type.getClassFields(Config)){
+			if(RestrictedUtils.hasMetadata(Config, field, "configParam")){
 				configList.push(field);
 			}
 		}
