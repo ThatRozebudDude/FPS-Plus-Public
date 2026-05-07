beb7f676287f46b8724aa95944b882d44b3372ac
lasystuff <soushimiyatani0210@gmail.com>
2026-05-07 17:55:03 +0900

    Allow the player to continue controlling even after the song ends
    Sometimes the chart editor crashes when I try to open it during player is singing, and it seems this is the cause...

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -340,8 +340,6 @@ class PlayState extends MusicBeatState
 	public static var sectionStart:Bool =  false;
 	public static var sectionStartTime:Float =  0;
 
-	var endingSong:Bool = false;
-
 	public var forceCenteredNotes:Bool = false;
 
 	public var meta:SongMetaTags;
@@ -1570,7 +1568,7 @@ class PlayState extends MusicBeatState
 			}
 		}
 
-		if (!inCutscene && !endingSong){
+		if (!inCutscene){
 		 	if(!autoplay){
 		 		keyShit();
 			}
@@ -1992,7 +1990,6 @@ class PlayState extends MusicBeatState
 	function stopMusic():Void{
 		songEnded = true;
 		canPause = false;
-		endingSong = true;
 		FlxG.sound.music.volume = 0;
 		FlxG.sound.music.pause();
 		vocals.volume = 0;
